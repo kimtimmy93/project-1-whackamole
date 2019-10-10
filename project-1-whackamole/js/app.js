c423b15aa3c7f8a6202d1debd3b2ca87da31a5e6
kimtimmy93 <kimtimmy93@gmail.com>
2019-10-10 00:24:02 -0700

    Commit 4 - Moles pop up at random

--- project-1-whackamole/js/app.js
+++ project-1-whackamole/js/app.js
@@ -1,28 +1,26 @@
 $('#container').on('mousedown', (e) => {
     $('#container').css('cursor', 'url(images/hammer2.png), crosshair');
-        if(!e.isTrusted){
-        return;
-    }
 })
 
 $('#container').on('mouseup', () => {
     $('#container').css('cursor', 'url(images/hammer.png), crosshair');
 })
 
-$('#timer').hide();
-$('#points').hide();
-$('.hole').hide();
-$('.clouds').hide();
+// $('#timer').hide();
+// $('#points').hide();
+// $('.hole').hide();
+// $('.clouds').hide();
 
 $('#start').on('click', () => {
-    $('#timer').show();
-    $('#points').show();
-    $('.hole').show();
-    $('.clouds').show();
+    // $('#timer').show();
+    // $('#points').show();
+    // $('.hole').show();
+    // $('.clouds').show();
     game.setTimer();
     game.setShowMoles();
     game.setPeepTime();
     game.setPeepHoles();
+    game.setHit();
    
 }); 
 
@@ -44,34 +42,39 @@ const game = {
     },
     setShowMoles(){
         //random time for moles to peep up for
-    //    const $mole = $('#mole1')
-    //    $mole.attr('src', 'images/mole.png')
+       const time = this.setPeepTime(200, 1000);
+       const holes = this.setPeepHoles();
+        $(holes).attr('src','images/mole.png')
+       setTimeout(() => {
+        $(holes).attr('src','images/hole.png')
+       },time);
 
-       const time = this.setPeepTime(500, 1000);
-       const hole = this.setPeepHoles();
-       console.log(time, hole);
+    //    if(points === 10){
+
+    //    }
     },
     setPeepTime(min, max){
         // set a random time for moles to appear
        return Math.round(Math.random()*(max - min)+ min);
+       
     },
     setPeepHoles(){
-        let lastHole;
+        let sameHole;
         // set a random hole that will where mole will peep up from
-        const $hole = $('.hole');
+        const $hole = $('.mole');
         const index = Math.floor(Math.random()*$hole.length);
         const randHole = $hole[index];
-        if(randHole === lastHole) {
+        if(randHole === sameHole) {
             console.log('same hole')
             return this.setPeepHoles();
         }
-        lastHole = randHole;
+        sameHole = randHole;
         return randHole;
-    
     },
-    setHit(){
+    setHit(e){
         // if hammer touches mole img count it as a hit
-        
+        // console.log(e.target)
+        // console.log(e.currentTarget)
     }
      
 }
